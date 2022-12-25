4a1900d1fe90653e6b051b4b4aa6c47b1e3f110a
dsbjzxnc <105147425+dsbjzxnc@users.noreply.github.com>
2022-12-25 17:03:48 +0800

    Add files via upload

---
+++ 网页/new_file.vue
@@ -0,0 +1,13 @@
+// 设置导航栏监听事件vue
+// 1创建页面滚动的事件监听
+// 2获取固定在顶部的页面元素
+// 实现元素固定在页面顶部的效果(当页面滚动时候要固定在页面顶部(触碰到页面顶部时))
+<!-- 教学视频地址C:\Users\Administrator\Videos\vue -->
+<template>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
